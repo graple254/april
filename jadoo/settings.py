a30aed48cff3acbb2b9f77dbbf0055478d57d665
graple254 <graple254@gmail.com>
2025-05-01 13:39:37 +0300

    aws keys to env take 2

--- jadoo/settings.py
+++ jadoo/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-import os
 from dotenv import load_dotenv
+import os
+load_dotenv()  # this reads your .env file and sets environment variables
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
