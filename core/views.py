73ff0c95a8701e2850383f9c0143a7d3961d4c26
graple254 <graple254@gmail.com>
2025-05-09 13:24:02 +0300

    updated the landing page 13.23

--- core/views.py
+++ core/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import *
 
-
 def index(request):
-    return render(request, 'index.html')
+    # Fetch the latest hero, platform details, and contact information
+    hero_info = hero.objects.last()  # Get the latest Hero object
+    platform_details = platform_detail.objects.all()  # Get all Platform details
+    contact_info = Contact.objects.first()  # Get the first Contact object (assuming you only have one)
+    social_media_info = social_media.objects.all()  # Get all Social Media objects
+
+    context = {
+        'hero_info': hero_info,
+        'platform_details': platform_details,
+        'contact_info': contact_info,
+        'social_media_info': social_media_info,
+    }
+    
+    return render(request, 'index.html', context)
